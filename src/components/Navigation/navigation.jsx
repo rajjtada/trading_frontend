7e2d813ba3a03699803108a6d2b8b5bbcc6525a1
shahzealc <zeal532003@gmail.com>
2024-08-31 00:08:55 +0530

    Integrated kotak and finvasia with api and maintained cookies for them and made popup for otp page for kotak and success page for finvasia

--- src/components/Navigation/navigation.jsx
+++ src/components/Navigation/navigation.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import { Navbar, Nav, NavDropdown, Offcanvas } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import "./navigation.css"
@@ -9,6 +9,8 @@ import apiBaseUrl from "../../constants/globaldata";
 import loader from "../../assets/images/loader.gif";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCheckCircle, faTimesCircle } from '@fortawesome/free-solid-svg-icons';
+import KotakOtpModal from '../Integration/kotak/kotak_otp';
+import Finvasia from '../Integration/finvasia/finvasia_modal';
 import Cookies from "js-cookie";
 
 export default function Navigation() {
@@ -17,20 +19,32 @@ export default function Navigation() {
     const [loading, setLoading] = useState(false);
     const [isKotakIntegrated, setIsKotakIntegrated] = useState(false);
     const [isFinvasiaIntegrated, setIsFinvasiaIntegrated] = useState(false);
+    const [showKotakModal, setShowKotakModal] = useState(false);
+    const [showFinvasiaModal, setShowFinvasiaModal] = useState(false);
+
+    useEffect(() => {
+        if (Cookies.get('kotak_access_token')) {
+            setIsKotakIntegrated(true);
+        }
+        if (Cookies.get('finvasia_access_token')) {
+            setIsFinvasiaIntegrated(true);
+        }
+    }, []);
+
     const navigation = useNavigate();
 
     const toggleOffcanvas = () => {
         setShow(!show);
     };
 
-    const KotakIntegration = () => {
+    const handleKotakIntegration = () => {
         setLoading(true);
         axios
             .get(
-                `${apiBaseUrl}integration/kotak/`,
-                {   
+                `${apiBaseUrl}integrations/kotak/`,
+                {
                     headers: {
-                        Cookie: Cookies.get("access_token"), 
+                        Authorization: Cookies.get("access_token"),
                         'bypass-tunnel-reminder': "true"
                     }
                 }
@@ -38,71 +52,64 @@ export default function Navigation() {
             .then((res) => {
                 setLoading(false);
                 if (res.status === 200) {
-                    const accessToken = res.data["access_token"];
-                    // Cookies.set('access_token', accessToken, { expires: 1 }); 
-                    toast.success("Login Successful");
-                    setIsKotakIntegrated(true);
-                    navigation("/kotak-auth");
+                    localStorage.setItem('access_token', res.data["access_token"]);
+                    localStorage.setItem('sid', res.data["sid"]);
+                    localStorage.setItem('user_id', res.data["user_id"]);
+                    localStorage.setItem('view_token', res.data["view_token"]);
+                    setShowKotakModal(true);
                 }
             })
             .catch((error) => {
                 setLoading(false);
                 if (error.response) {
-                    if (error.response.status === 400) {
-                        toast.error("OTP Invalid !");
-                    } else {
-                        toast.error(`Error: ${error.response.status} - ${error.response.data.message || 'Something went wrong!'}`);
-                    }
+                    toast.error(`Error: ${error.response.status} - ${error.response.data.message || 'Something went wrong!'}`);
                 } else if (error.request) {
                     toast.error('No response received from the server.');
                 } else {
-                    toast.error('There was an error verifying the OTP!');
+                    toast.error('Something went wrong');
                 }
             });
-            setIsKotakIntegrated(!isKotakIntegrated);
+
     }
 
-    const FinvasiaIntegration = () => {
+    const handleFinvasiaIntegration = () => {
         setLoading(true);
         axios
             .get(
-                `${apiBaseUrl}integration/finvasia/`,
-                {   
-
+                `${apiBaseUrl}integrations/shoonya/`,
+                {
                     headers: {
-                        Cookie: Cookies.get("access_token"),
+                        Authorization: Cookies.get("access_token"),
                         'bypass-tunnel-reminder': "true"
                     }
                 }
             )
             .then((res) => {
                 setLoading(false);
+                console.log(res);
                 if (res.status === 200) {
-                    const accessToken = res.data["access_token"];
-                    // Cookies.set('access_token', accessToken, { expires: 1 }); 
-                    toast.success("Login Successful");
-                    setIsFinvasiaIntegrated(true)
-                    navigation("/finvasia-auth");
+                    Cookies.set('finvasia_access_token', res.data, { expires: 1 });
+                    setShowFinvasiaModal(true);
+                    setIsFinvasiaIntegrated(true);
                 }
             })
             .catch((error) => {
                 setLoading(false);
                 if (error.response) {
                     if (error.response.status === 400) {
-                        toast.error("OTP Invalid !");
+                        toast.error("Invalid Parameter!");
                     } else {
                         toast.error(`Error: ${error.response.status} - ${error.response.data.message || 'Something went wrong!'}`);
                     }
                 } else if (error.request) {
                     toast.error('No response received from the server.');
                 } else {
-                    toast.error('There was an error verifying the OTP!');
+                    toast.error('Something went wrong');
                 }
             });
-            setIsFinvasiaIntegrated(!isFinvasiaIntegrated);
     }
 
-    const Already_Integrated = () => {
+    const handleAlreadyIntegrated = () => {
         toast.info("Already Integrated");
     }
 
@@ -127,44 +134,38 @@ export default function Navigation() {
                             <NavDropdown.Item href="#action/3.2">Option2</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Option3</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link onClick={isKotakIntegrated ? Already_Integrated : KotakIntegration} className={isKotakIntegrated ? "kotak_btn_integrated" : "kotak_btn"}>
-                            Kotak
-                        <FontAwesomeIcon
-                                icon={isKotakIntegrated ? faCheckCircle : faTimesCircle}
-                                style={{ marginLeft: '8px', color: isKotakIntegrated ? 'green' : 'red' }}
-                            />
+                        <Nav.Link onClick={isKotakIntegrated ? handleAlreadyIntegrated : handleKotakIntegration} className={isKotakIntegrated ? "kotak_btn_integrated" : "kotak_btn"}>
+                            Kotak {isKotakIntegrated ? <FontAwesomeIcon icon={faCheckCircle} className="check-icon" /> : <FontAwesomeIcon icon={faTimesCircle} className="cross-icon" />}
                         </Nav.Link>
-
-                        <Nav.Link onClick={isFinvasiaIntegrated ? Already_Integrated : FinvasiaIntegration} className={isFinvasiaIntegrated ? "finvasia_btn_integrated" : "finvasia_btn"}>
-                            Finvasia
-                        <FontAwesomeIcon
-                                icon={isFinvasiaIntegrated ? faCheckCircle : faTimesCircle}
-                                style={{ marginLeft: '8px', color: isFinvasiaIntegrated ? 'green' : 'red' }}
-                            />
+                        <Nav.Link onClick={isFinvasiaIntegrated ? handleAlreadyIntegrated : handleFinvasiaIntegration} className={isFinvasiaIntegrated ? "finvasia_btn_integrated" : "finvasia_btn"}>
+                            Finvasia {isFinvasiaIntegrated ? <FontAwesomeIcon icon={faCheckCircle} className="check-icon" /> : <FontAwesomeIcon icon={faTimesCircle} className="cross-icon" />}
                         </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
-
             <Offcanvas show={show} onHide={toggleOffcanvas}>
                 <Offcanvas.Header closeButton>
-                    <Offcanvas.Title>Dashboard</Offcanvas.Title>
+                    <Offcanvas.Title>Menu</Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
-                    <Nav className="flex-column">
+                    <Nav className="ml-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
-                        <NavDropdown title="More" id="offcanvas-nav-dropdown">
+                        <Nav.Link href="#features">Portfolio</Nav.Link>
+                        <Nav.Link href="#pricing">Orders</Nav.Link>
+                        <NavDropdown title="More" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Option1</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Option2</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Option3</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link onClick={KotakIntegration} className="kotak_btn">Kotak</Nav.Link>
-                        <Nav.Link onClick={FinvasiaIntegration} className="finvasia_btn">Finvaisa</Nav.Link>
                     </Nav>
                 </Offcanvas.Body>
             </Offcanvas>
+
+            {/* Kotak OTP Modal */}
+            <KotakOtpModal show={showKotakModal} setIsKotakIntegrated={setIsKotakIntegrated} onHide={() => setShowKotakModal(false)} />
+
+            {/* Finvasia Success Modal */}
+            <Finvasia show={showFinvasiaModal} onHide={() => setShowFinvasiaModal(false)} />
         </>
     );
 }
