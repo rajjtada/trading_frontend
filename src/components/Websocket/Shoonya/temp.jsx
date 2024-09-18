e71f9090c58b8334c1530e6c47674e9f4fb97eaf
shaileshdarji123 <shaileshdarji1321@gmail.com>
2024-09-18 13:53:27 +0530

    Solved bug for realtime data in hs library and subscribe to temp scrip

--- src/components/Websocket/Shoonya/temp.jsx
+++ src/components/Websocket/Shoonya/temp.jsx
@@ -1,68 +1,45 @@
 /* eslint-disable */
 import React, { useEffect, useState } from 'react';
 import Cookies from 'js-cookie';
-// import "../hslib.js"
-// import {HSIWebSocket} from '../hslib.js';
+import HSWebSocket from '../hslib';
 const WebSocketComponent = () => {
   const [isConnected, setIsConnected] = useState(false);
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    // const url = "wss://mlhsi.kotaksecurities.com/realtime?sId=server1";
-    // ws = new HSIWebSocket(url)
-    // const myScript = document.createElement('script');
-    // myScript.src = '../hslib.js';
-    // document.body.appendChild(myScript);
-        // if (HSIWebSocket) {
-        //   console.log('HSIWebSocket is loaded and ready to use.');
+    let handshakeServerId = 'server7'
+    let token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzY29wZSI6WyJUcmFkZSJdLCJleHAiOjE3MjY2ODQyMDAsImp0aSI6Ijg2YWRiNzk3LWY2YWItNDBiNS05YWQ4LTQ4ZjE3NGQ2OWYxOSIsImlhdCI6MTcyNjY0MjI1OSwiaXNzIjoibG9naW4tc2VydmljZSIsInN1YiI6ImM0MDRhMWZjLTMzZjMtNDBmNC04YzM1LWNhMDk5ZTRjNTY2OCIsInVjYyI6IllUV0pPIiwibmFwIjoiIiwieWNlIjoiZVlcXDYgXCI5NT13XHUwMDA2XGZcdTAwMDN9XHUwMDAwXHUwMDEwYiIsImZldGNoY2FjaGluZ3J1bGUiOjAsImNhdGVnb3Jpc2F0aW9uIjoiIn0.giYlgCsCVguJLTle5fDck7N87u_1YdwilZbtCdCDUMj0z2ByeWJLymjWlU7MiiCjyFaYHc7yEsMxxDQtp23Zbg1CgrkjdY5KpPq1tTbYYHKO6fF1jLatDg2Vj4-qj1SzzrJFEnenzxTjiDcyy7l_EkZM3gpnVxaOAURGTtg3dOZyz60oRz71kNGvq2ZImmiVWLma3Qc9gyLatHwmnVZpG59RZcYTg86nIt07XjtaS3ydBqS0mqt-Dubs1pMaiBq8AUr6yjA-T-2mik0US7c3HDKKYakYEWbN6e0N2qWrSafx80Wl4LXX8wmccfRnTCPhEWxI-b6kJ7MyJxVWxx856g"
+    let sid = "314a26d2-19fb-465d-8c79-be2b511ac35d"
 
-        //   // Function to connect to WebSocket
-        //   const connectHsi = (token, sid, handshakeServerId) => {
-        //     const hsWs = new window.HSIWebSocket(url);
+    let url = "wss://mlhsm.kotaksecurities.com";
+    let userWS = new HSWebSocket(url);
+    // console.log(document.getElementById('channel_number').value)
 
-        //     hsWs.onopen = () => {
-        //       console.log('[Socket]: Connected to "' + url + '"');
-        //       const hsijObj = {
-        //         type: "cn",
-        //         Authorization: token,
-        //         Sid: sid,
-        //         source: "WEB"
-        //       };
-        //       hsWs.send(JSON.stringify(hsijObj));
-        //       setIsConnected(true);
-        //     };
 
-        //     hsWs.onclose = () => {
-        //       console.log("[Socket]: Disconnected !");
-        //       setIsConnected(false);
-        //     };
+    userWS.onopen = function () {
+        console.log('[Socket]: Connected to "' + url + '"\n');
+        let jObj = {};
+        jObj["Authorization"] = token;
+        jObj["Sid"] = sid; 
+        jObj["type"] = "cn";
+        userWS.send(JSON.stringify(jObj));
 
-        //     hsWs.onerror = () => {
-        //       console.log("[Socket]: Error !");
-        //       setIsConnected(false);
-        //     };
+        jObj = {"type":'mws', "scrips":"nse_cm|11536&nse_cm|1594&nse_cm|3456", "channelnum":"1"};
+        userWS.send(JSON.stringify(jObj));
+    }
 
-        //     hsWs.onmessage = (msg) => {
-        //       console.log('[Res]: ', msg.data);
-        //       setMessages((prevMessages) => [...prevMessages, msg.data]);
-        //     };
+    userWS.onclose = function () {
+        console.log("[Socket]: Disconnected !\n");
+    }
 
-        //     return hsWs;
-        //   };
+    userWS.onerror = function () {
+        console.log("[Socket]: Error !\n");
+    }
 
-        //   // Get cookies and connect to the WebSocket
-        //   const token = JSON.parse(Cookies.get("kotak_access_token")).auth;
-        //   const sid = JSON.parse(Cookies.get("kotak_access_token")).sid;
-        //   const handshakeServerId = JSON.parse(Cookies.get("kotak_access_token")).sld;
-
-        //   const ws = connectHsi(token, sid, handshakeServerId);
-
-        //   return () => {
-        //     ws.close(); // Clean up the WebSocket connection
-        //   };
-        // } else {
-        //   console.error("HSIWebSocket is not available!");
-        // }
+    userWS.onmessage = function (msg) {
+        const result= JSON.parse(msg);
+        console.log('[Res]: ' + msg + "\n");
+    }
    
       
   }, []); // Runs once on component mount
